4e6b8937ebfc8a4ee3b1cd219767f5ac5782b848
dianayossi <dycarachure@gmail.com>
2022-07-27 00:30:16 -0700

    lesson 4-2

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const today = new Date();
+const thisYear = today.getFullYear();
+
+const footer = document.querySelector('footer');
+
+const copyright = document.createElement('p');
+
+copyright.innerHTML = `Diana Carachure ${'thisYear'}`;
+
+footer.appendChild(copyright);
+
+const skills = ['JavaScript', 'HTML', 'CSS'];
+const skillsSection = document.getElementById('skills');
+const skillsList = skillsSection.querySelector('ul'); //replace document with skillsSection -only "looking" in that section
+
+for (let i = 0; i < skills; i++){
+    const skill = document.createElement('li');
+    skill.innerHTML = `${skills}`;
+    skillsList.appendChild(skill);
+};
