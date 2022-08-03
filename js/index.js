8ea7448c991839084613f35ba5b733372e3613f1
dianayossi <dycarachure@gmail.com>
2022-08-02 23:15:17 -0700

    lesson 4-3

--- js/index.js
+++ js/index.js
@@ -9,12 +9,48 @@ copyright.innerHTML = `Diana Carachure ${thisYear}`;
 
 footer.appendChild(copyright);
 
-const skills = ['JavaScript', 'HTML', 'CSS'];
+const skills = ['JavaScript', 'HTML', 'CSS', 'Access', 'Excel','Data Entry', 'Apricot by Social Solutions'];
 const skillsSection = document.getElementById('skills');
 const skillsList = skillsSection.querySelector('ul'); //replace document with skillsSection -only "looking" in that section
 
-for (let i = 0; i < skills; i++){
+for (let i = 0; i < skills.length; i++){
     const skill = document.createElement('li');
-    skill.innerHTML = `${skills}`;
+    skill.innerText = skills[i];
     skillsList.appendChild(skill);
 };
+
+//start of lesson-4-3
+
+const messageForm = document.getElementsByName('leave_message');
+
+messageForm.addEventListener('submit', (event) => {
+        event.preventDefault(); //values should be logging in the console.
+
+        const name = event.target.name; //changed const to let since string will change
+        const email = event.target.email;
+        const message = event.target.message;
+
+        console.log(name);
+        console.log(email);
+        console.log(message);
+
+        const messageSection = document.getElementById('messages');
+        const messageList = messageSection.querySelector('ul'); 
+        const newMessage = document.createElement('li');
+
+         newMessage.innerHTML = `<a href = mailto: ${email}> ${name}</a> Wrote : <span> ${message} </span>`;
+
+        const removeButton = document.createElement('button');
+        removeButton.innerText = 'remove';
+        removeButton.type = 'button';
+
+        removeButton.addEventListener('click', () => {
+            const entry = removeButton.parentNode;
+            entry.remove();
+        });
+
+        newMessage.appendChild('removeButton');
+        messageList.appendChild('newMessage');
+
+        event.target.reset();
+});
