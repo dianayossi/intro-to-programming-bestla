8e022cc7ff50a4db2f17463508ebfe8ac95b5528
dianayossi <dycarachure@gmail.com>
2022-08-07 12:23:09 -0700

    revisiting code

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const today = new Date();
-const thisYear = today.getFullYear();
+let thisYear = today.getFullYear();
 
 const footer = document.querySelector('footer');
 
@@ -14,13 +14,13 @@ const skillsSection = document.getElementById('skills');
 const skillsList = skillsSection.querySelector('ul'); //replace document with skillsSection -only "looking" in that section
 
 for (let i = 0; i < skills.length; i++){
-    const skill = document.createElement('li');
+    let skill = document.createElement('li');
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 };
 
 
-//start of lesson-4-3
+/*start of lesson-4-3
 
 let messageForm = document.getElementsByName('leave_message');
 
@@ -58,3 +58,45 @@ messageForm.addEventListener('submit', (e) => {
 
         e.target.reset();
 ;
+*/
+
+//code below will handle the messages submitted in the form section of the page
+let messageForm = document.getElementsByName("leave_message");
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+//retrieving the value from each event
+let name = e.target.name.value;
+let email = e.target.email.value;
+let message = e.target.message.value;
+
+console.log(name);
+console.log(email);
+console.log(message);
+
+//is not logging values in console when it should be
+
+//following section will display messages in a list
+
+let messageSection = document.getElementById("messages");
+let messageList = messageSection.querySelector("ul");
+let newMessage = document.createElement("li");
+
+newMessage.innerHTML = `<a href= "mailto: ${email}"> ${name} </a> wrote: <span> ${message} </span>`;
+
+//creates a new buttom named remove
+
+let removeButton = document.createElement("button");
+removeButton.innerText = "remove";
+removeButton.type = "button";
+removeButton.addEventListener('click', () => {
+    let entry = removeButton.parentNode;
+    entry.remove();
+});
+
+newMessage.appendChild(removeButton);
+
+messageList.append(messageList);
+
+e.target.reset();
+});
