10d72b81e88ae9e033be39de9ef8c777f19e9be2
dianayossi <dycarachure@gmail.com>
2022-08-06 21:55:04 -0700

    revisiting code

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
+//will get current year
 const today = new Date();
-const thisYear = today.getFullYear();
+let thisYear = today.getFullYear();
 
-const footer = document.querySelector('footer');
+const footer = document.querySelector("footer");
 
-const copyright = document.createElement('p');
+//creating a new element "paragraph" <p>
+const copyright = document.createElement("p");
 
 copyright.innerHTML = `Diana Carachure ${thisYear}`;
 
+//will make name and year from line 9 show up in footer
 footer.appendChild(copyright);
 
-const skills = ['JavaScript', 'HTML', 'CSS']; 
-const skillsSection = document.getElementById('skills');
-const skillsList = skillsSection.querySelector('ul'); //replace document with skillsSection -only "looking" in that section
+let skills = ['JavaScript', 'HTML', 'CSS']; 
+let skillsSection = document.getElementById("skills");
+let skillsList = skillsSection.querySelector("ul"); //replace document with skillsSection -only "looking" in that section
 
 for (let i = 0; i < skills.length; i++){
-    const skill = document.createElement('li');
+    let skill = document.createElement("li");
     skill.innerHTML = `${skill}`; //skills are not showing up
     skillsList.appendChild(skill);
 };
