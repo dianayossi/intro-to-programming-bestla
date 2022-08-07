5922a2c0eadd4f4b0651a356ef542c07f23bc1a2
dianayossi <dycarachure@gmail.com>
2022-08-07 12:36:10 -0700

    updating changes to lesson-4-3

--- js/index.js
+++ js/index.js
@@ -19,56 +19,15 @@ for (let i = 0; i < skills.length; i++){
     skillsList.appendChild(skill);
 };
 
-
-/*start of lesson-4-3
-
-let messageForm = document.getElementsByName('leave_message');
-
-messageForm.addEventListener('submit', (e) => {
-        e.preventDefault(); //values should be logging in the console.
-
-    });
-
-        const name = e.target.name.value; //changed const to let since string will change
-        const email = e.target.email.value;
-        const message = e.target.message.value;
-
-        console.log('name');
-        console.log('email');
-        console.log('message');
-
-        const messageSection = document.getElementById('messages');
-        const messageList = messageSection.querySelector('ul'); 
-        const newMessage = document.createElement('li');
-        const messageSection = document.getElementById('messages');
-
-         newMessage.innerHTML = `<a href = mailto: ${email}> ${name}</a> Wrote : <span> ${message} </span>`;
-
-        const removeButton = document.createElement('button');
-        removeButton.innerText = 'remove';
-        removeButton.type = 'button';
-
-        removeButton.addEventListener('click', () => {
-            const entry = removeButton.parentNode;
-            entry.remove();
-        });
-
-        newMessage.appendChild('removeButton');
-        messageList.appendChild('newMessage');
-
-        e.target.reset();
-;
-*/
-
 //code below will handle the messages submitted in the form section of the page
 let messageForm = document.getElementsByName("leave_message");
 messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
 //retrieving the value from each event
-let name = e.target.name.value;
-let email = e.target.email.value;
-let message = e.target.message.value;
+let name = e.target.name;
+let email = e.target.email;
+let message = e.target.message;
 
 console.log(name);
 console.log(email);
